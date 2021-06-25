ee54f0d6addb8b1be02ebb24d7a54efd2ea93c78
George Zotta <zottamendez@hotmail.com>
2021-06-25 14:28:44 +1200

    added the "getBadgeClasses" methods to render classes dyncamically depending on the  value of the "count" property.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,20 +5,19 @@ class Counter extends Component {
     count: 0,
   };
 
-  styles = {
-    fontSize: "50px",
-    fontWeight: "bold",
-  };
-
   render() {
     return (
       <React.Fragment>
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
@@ -28,3 +27,10 @@ class Counter extends Component {
 }
 
 export default Counter;
+
+/*Notes:
+- React.Fragment is to group a list of children without adding extra nodes to the DOM.
+- Some bootstrap classes used in this code: "badge" is to style the label that displays the count. "badge-primary" is to make the badge blue. "badge-warning" is to make the badge yellow. 
+  "m-2" is to set the margin. Class "btn" is a button and "btn-sm" is a small button. 
+- getBadgeClasses() is to render the classes depending on the value of the "count" property, yellow if it's zero otherwise blue.
+*/
