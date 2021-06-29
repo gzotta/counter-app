4420f5be7308192a73a55d3eb3017651dc47a9db
George Zotta <zottamendez@hotmail.com>
2021-06-29 19:06:51 +1200

    Counter class modified.
    count property from Counter class was changed to value and set to "this.props.value"
    to capture the value  from the "value" field from the "counters" property from
    state of the Counters class.
    #8

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
   //this.setState() it's inherited from React's component. This method tells React that we are updating the state, then it'll figure out what part of the state is changed, and based on that it'll bring the DOM in sync with the virtual DOM.
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    // console.log("props", this.props);
+
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -25,12 +27,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
